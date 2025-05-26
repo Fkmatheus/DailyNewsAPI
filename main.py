6a7096ec522b54a9d1a078cf66a03c09fea14ca6
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-05-26 20:53:51 -0300

    Puxando titulo e descrição das noticias

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import requests
 
 api_key = "pub_c127641ebc1b4a1aac153d1287725a7b"
 
-url = " https://newsdata.io/api/1/latest?apikey=pub_c127641ebc1b4a1aac153d1287725a7b&q=pizza"
+url = "https://newsdata.io/api/1/latest?apikey=pub_c127641ebc1b4a1aac153d1287725a7b&q=pizza"
 
 request = requests.get(url)
 content = request.json()
 
-print(content)
+#print(content["results"])
+
+cont = 0
+for x in content["results"]:
+    print(content["results"][cont]["title"])
+    print('--------------------------------------------')
+    print(content["results"][cont]["description"])
+    print()
+    cont += 1
