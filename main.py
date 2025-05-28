ae1ab36bf59cc0889ed78b94e7455cfe8813ed6d
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-05-27 21:59:21 -0300

    Inserindo a função de enviar as noticias por email

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "pub_c127641ebc1b4a1aac153d1287725a7b"
 
@@ -9,10 +10,14 @@ content = request.json()
 
 #print(content["results"])
 
+body = ""
 cont = 0
 for x in content["results"]:
-    print(content["results"][cont]["title"])
-    print('--------------------------------------------')
-    print(content["results"][cont]["description"])
-    print()
+    if content["results"][cont]["title"] != None and content["results"][cont]["description"] != None and content["results"][cont]["link"] != None:
+        body = body + content["results"][cont]["title"] + "\n" + content["results"][cont]["description"] + "\n" + content["results"][cont]["link"] + 2*"\n"
+
     cont += 1
+
+body = body.encode("utf-8")
+send_email(body)
+
